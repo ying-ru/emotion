9810c9e685a6014ba8acab99d8c29c5eb51e0c58
ying-ru <sapphireblue23@gmail.com>
2014-08-13 01:45:16 +0800

    add fuzzy (待改)

---
+++ src/Fuzzy/EmotionFuzzy.java
@@ -0,0 +1,34 @@
+package Fuzzy;
+
+import net.sourceforge.jFuzzyLogic.FIS;
+import net.sourceforge.jFuzzyLogic.FunctionBlock;
+
+public class EmotionFuzzy {
+	public static void main(String[] args) throws Exception {
+		String filename = "PAD.fcl";
+		FIS fis = FIS.load(filename, true);
+
+		
+		if (fis == null) {
+			System.err.println("Can't load file: '" + filename + "'");
+			System.exit(1);
+		}
+
+		// Get default function block
+		FunctionBlock fb = fis.getFunctionBlock(null);
+
+		// Set inputs
+		fb.setVariable("dirty", 60);
+		fb.setVariable("amt", 70);
+
+		// Evaluate
+		fb.evaluate();
+
+		// Show output variable's chart
+		fb.getVariable("time").defuzzify();
+
+		// Print ruleSet
+		System.out.println(fb);
+		System.out.println("Time: " + fb.getVariable("time").getValue());
+	}
+}
